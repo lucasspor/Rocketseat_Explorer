f58295b180125dc89cb81eb6d4ff526a06411808
lucasspor <lucassporto.contato@gmail.com>
2024-09-08 08:43:02 -0300

    feat: learning about routes, location and destructuring

---
+++ Javascript/Antes_do_Framework/projetos/projeto_07/src/index.js
@@ -0,0 +1,25 @@
+
+const router = {
+  "/": "/pages/home.html",
+  "/about": "/pages/about.html",
+  "/contact": "/pages/contact.html",
+  404: "/pages/404.html"
+}
+document.querySelectorAll('nav a').forEach(link =>  {
+  link.addEventListener('click', route)
+});
+
+function route(event){
+  event = event || window.event
+  event.preventDefault()
+
+  window.history.pushState({}, "", event.target.href)
+
+  handle()
+}
+
+function handle(){
+  const { pathname } = window.location
+}
+
+
