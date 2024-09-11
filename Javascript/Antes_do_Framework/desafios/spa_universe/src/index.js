40d71daaf865143768d2ef5d5dc71983b7bb6c2c
lucasspor <lucassporto.contato@gmail.com>
2024-09-11 13:35:04 -0300

    feat: Adding routes and chenging back and stylesheet

--- Javascript/Antes_do_Framework/desafios/spa_universe/src/index.js
+++ Javascript/Antes_do_Framework/desafios/spa_universe/src/index.js
@@ -1,3 +1,56 @@
-import Router from "./routes";
+const app = document.querySelector("#app")
+const main = document.querySelector("main")
 
-const router = new Router()
+
+const routes = {
+  "/": "/pages/home/home.html",
+  "/universe": "/pages/universe/universe.html",
+   "/exploration": "/pages/exploration/exploration.html"
+}
+
+
+
+document.querySelectorAll(".navigation a").forEach(link => {
+  link.addEventListener('click', router)
+})
+
+function router(event) {
+  event = event || window.event
+  event.preventDefault()
+  
+  window.history.pushState({}, "", event.target.href)
+
+  handle()
+}
+
+
+function handle(){
+  const {pathname} = window.location
+  const route = routes[pathname] 
+
+  fetch(route).then(data => data.text().then(html =>{
+    document.querySelector('#content').innerHTML = html
+  }  ))
+
+  console.log(route)
+}
+
+function updateBackground() {
+  if (main.querySelector('#universe')) {
+    app.style.backgroundImage = 'url("/assets/mountains-universe-2.png")';
+  } else  if (main.querySelector('#exploration')) {
+    app.style.backgroundImage = 'url("/assets/mountains-universe-3.png")'
+  } else{
+    app.style.backgroundImage = ''
+  }
+}
+
+updateBackground();
+
+const observer = new MutationObserver(() => {
+  updateBackground();
+});
+
+observer.observe(main, { childList: true, subtree: true });
+
+handle()
