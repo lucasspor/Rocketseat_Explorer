74503eb1c86d2f1ebb1bc6442b3e9cb02c1797cc
lucasspor <lucassporto.contato@gmail.com>
2024-09-12 14:16:02 -0300

    feat: adding empty fav githubs, and finishing adding links gitfavortites

--- Javascript/Antes_do_Framework/projetos/github-favorites/src/favorites.js
+++ Javascript/Antes_do_Framework/projetos/github-favorites/src/favorites.js
@@ -1,3 +1,5 @@
+import { GithubUser } from "./githubUser.js";
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
@@ -6,19 +8,35 @@ export class Favorites {
   }
 
   load() {
-    this.entries = [{
-      login: 'lucasspor',
-      name: "Lucas Silva Porto",
-      public_repos: '8',
-      followers: '1'
-    },
-    {
-      login: 'codemilio',
-      name: "Carlos Emilio",
-      public_repos: '28',
-      followers: '14'
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
+    this.update()   
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+
+  async add(username) {
+    try {
+      const user = await GithubUser.search(username)
+      const userExists = this.entries.find(entry => entry.login === user.login)
+
+      if (user.login === undefined) {
+        throw new Error('Usuario não encontrado!')
+      }
+
+      if (userExists) {
+        throw new Error('Usuário já adicionado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
     }
-    ]
+
   }
 
   delete(user) {
@@ -27,6 +45,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -35,10 +54,35 @@ export class FavoritesView extends Favorites {
     super(root)
 
     this.update()
+    this.onAdd()
+  }
+
+  onAdd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+      const input = this.root.querySelector('.search input')
+      if (!value) {
+        return alert('Digite um  nome de usuario!!')
+      }
+
+      this.add(value)
+
+      input.value = ''
+    }
   }
 
   update() {
     this.removeAllTr()
+    if (this.entries.length === 0) {
+     return this.tbody.innerHTML = `
+        <tr>
+          <td align="center" colspan="${this.root.querySelectorAll('table thead th').length}" rowspan="">
+            <img src="https://media.tenor.com/k_hV6yJJrEoAAAAM/n%C3%A3o-tem-nada-pra-gente-fazer-aqui-n%C3%A3o-greg%C3%B3rio-duvivier.gif" alt="No data" style="display: block; margin: 0 auto;" />
+          </td>
+        </tr>
+      `;
+    }
 
     this.entries.forEach(user => {
       const row = this.createRow()
