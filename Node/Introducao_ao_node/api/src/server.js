558fbd213c7110620628ec952a6fc09dfe3ce347
lucasspor <lucassporto.contato@gmail.com>
2024-09-18 17:46:14 -0300

    feat: Using the get method to show Hello world

--- Node/Introducao_ao_node/api/src/server.js
+++ Node/Introducao_ao_node/api/src/server.js
@@ -3,4 +3,8 @@ const express = require("express")
 const app = express()
 const port = 3333
 
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
 app.listen(port, () => { console.log(`Server is running in Port ${port}`) })
