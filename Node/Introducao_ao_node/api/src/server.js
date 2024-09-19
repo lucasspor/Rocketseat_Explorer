856115c0dad90324cd24932a7445020562efaf6f
lucasspor <lucassporto.contato@gmail.com>
2024-09-19 08:58:14 -0300

    feat: Using concept of quey params

--- Node/Introducao_ao_node/api/src/server.js
+++ Node/Introducao_ao_node/api/src/server.js
@@ -3,10 +3,16 @@ const express = require("express")
 const app = express()
 const port = 3333
 
-app.get("/:id", (request, response) => {
+app.get("/:messages/:id", (request, response) => {
   const { id } = request.params
 
-  response.send(`Esta é o Id request: ${id}`)
+  response.send(`Este é o Id request: ${id}`)
 })
+
+app.get("/user", (request, response) => {
+  const {page, limit} = request.query
+
+  response.send(`Esta é sua pagina: ${page} e este seu limite ${limit}`)
+} )
 
 app.listen(port, () => { console.log(`Server is running in Port ${port}`) })
