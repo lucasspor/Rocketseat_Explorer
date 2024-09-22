6e5b7637619b5e773b40b20d3b4e0390fdd5ce27
lucasspor <lucassporto.contato@gmail.com>
2024-09-22 12:30:33 -0300

    feat: creating a create method in controller

--- Node/Introducao_ao_node/api/src/controllers/UsersController.js
+++ Node/Introducao_ao_node/api/src/controllers/UsersController.js
@@ -1,5 +1,9 @@
-class UsersController{
+class UsersController {
+  create(req, res) {
+    const { name, email, password } = req.body
 
+    res.json({ name, email, password })
+  }
 }
 
 module.exports = UsersController
