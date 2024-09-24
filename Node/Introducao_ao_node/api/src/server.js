420bcb4f0504468e12eef2258e210f6c4e6c2bdc
lucasspor <lucassporto.contato@gmail.com>
2024-09-23 23:25:13 -0300

    Feat: adding express-async-errors for treatment errors

--- Node/Introducao_ao_node/api/src/server.js
+++ Node/Introducao_ao_node/api/src/server.js
@@ -1,11 +1,30 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
 const express = require("express")
 
 const routes = require("./routes")
 
 const app = express()
 app.use(express.json())
-const port = 3333
 
 app.use(routes)
 
+app.use((error, req, res, next) => { 
+  if(error instanceof AppError){
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
+
+const port = 3333
 app.listen(port, () => { console.log(`Server is running in Port ${port}`) })
