94af0d11a58bdda677704c327dc1276df68a0771
lucasspor <lucassporto.contato@gmail.com>
2024-09-23 22:41:50 -0300

    feat: Adding middleware to usersRoutes

--- Node/Introducao_ao_node/api/src/routes/users.routes.js
+++ Node/Introducao_ao_node/api/src/routes/users.routes.js
@@ -4,8 +4,19 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
+function myMiddleware(req, res, next){
+  console.log("Você passou pelo middleware")
+  console.log(req.body)
+
+  if(!req.body.isAdmin){
+    return res.json({message: "You are not the admin"})
+  }
+
+  next()
+}
+
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create )
+usersRoutes.post("/", myMiddleware,usersController.create )
 
 module.exports = usersRoutes
