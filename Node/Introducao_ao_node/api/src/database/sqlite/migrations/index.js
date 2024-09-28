45f1615461e64ecba7b846b5965afbca2f5467ba
lucasspor <lucassporto.contato@gmail.com>
2024-09-28 14:33:02 -0300

    feat: adding migrations and createUsers migration to the project

---
+++ Node/Introducao_ao_node/api/src/database/sqlite/migrations/index.js
@@ -0,0 +1,13 @@
+const sqliteConnections = require("../../sqlite")
+const createUsers = require("./cresteUsers")
+
+async function migrationsRun(){
+  const schemas = [
+    createUsers,
+  ].join('')
+
+
+  sqliteConnections().then(db => db.exec(schemas)).catch(error => console.error(error))
+}
+
+module.exports = migrationsRun
