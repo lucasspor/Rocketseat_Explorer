7fefbf36d7e3d876ca76a3bf14bbc183874bcac3
lucasspor <lucassporto.contato@gmail.com>
2024-10-06 09:12:27 -0300

    Add: adding knex and creating a migration with knex

--- Node/Introducao_ao_node/api/knexfile.js
+++ Node/Introducao_ao_node/api/knexfile.js
@@ -6,7 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migration")
+    },
     useNullAsDefault: true
-  }
-
+  },
 };
