83691c1c2b2b30803d68d9d6dda2b30cb0e7efd3
lucasspor <lucassporto.contato@gmail.com>
2024-10-10 11:37:18 -0300

    feat: Adding migration for Links and tags, Adding function CASCADE to knexfile

--- Node/Introducao_ao_node/api/knexfile.js
+++ Node/Introducao_ao_node/api/knexfile.js
@@ -5,6 +5,8 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },pool:{
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migration")
