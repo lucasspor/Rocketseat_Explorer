4a96b0ef68b013bd710317a4e576d030c3a5601a
lucasspor <lucassporto.contato@gmail.com>
2024-10-13 20:05:57 -0300

    feat: a update method for project

--- Node/Introducao_ao_node/movie-api/src/controllers/UsersControler.js
+++ Node/Introducao_ao_node/movie-api/src/controllers/UsersControler.js
@@ -7,7 +7,7 @@ class UsersController {
     const { name, email, password } = req.body
 
     const database = await sqliteConnection()
-    
+
     const checkUserExist = await database.get("SELECT * FROM users WHERE email = ?", [email]);
 
     if (checkUserExist) {
@@ -25,14 +25,46 @@ class UsersController {
     const { name, email, password, old_password } = req.body
     const { id } = req.params
 
-    const database = sqliteConnection()
+    const database = await sqliteConnection()
 
-    const user = await database.run("SELECT * users WHERE id = (?)", [id])
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
     if (!user) {
-      throw new AppError("Usuário não existe")
+      throw new AppError("User doesn't exist")
     }
-    
+
+    const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso")
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (!password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova")
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (checkOldPassword) {
+        throw new AppError("A senha antiga não confere")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, user.id])
+
     return res.status(201).json({ message: "User has been updated!!" })
   }
 }
