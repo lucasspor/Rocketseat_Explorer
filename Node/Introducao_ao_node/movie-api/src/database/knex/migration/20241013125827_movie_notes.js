1c2febc59b593bf8f6c784188bad7101afaa153a
lucasspor <lucassporto.contato@gmail.com>
2024-10-13 11:05:53 -0300

    feat: adding migration for movies and user route create

--- Node/Introducao_ao_node/movie-api/src/database/knex/migration/20241013125827_movie_notes.js
+++ Node/Introducao_ao_node/movie-api/src/database/knex/migration/20241013125827_movie_notes.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = knex = knex.schema.createTable("movies", table => {
+  table.increments("id")
+  table.text("title")
+  table.text("description")
+  table.integer("rating").check("rating >= 1 AND rating <= 5")
 
-exports.down = function(knex) {
-  
-};
+  table.timestamp("created_at").default(knex.fn.now())
+  table.timestamp("updated_at").default(knex.fn.now())
+})
+
+exports.down = knex = knex.schema.dropTable("movies")
