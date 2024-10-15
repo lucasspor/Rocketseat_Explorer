3458d8f1579b38ecaaf8b1663326099e87240989
lucasspor <lucassporto.contato@gmail.com>
2024-10-15 19:41:13 -0300

    fix: Organizing the detail page component.

--- React/Rocket_notes/src/main.jsx
+++ React/Rocket_notes/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Details from './pages/Details.jsx'
+import Details from './pages/Details/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <Details/>
   </StrictMode>,
 )
