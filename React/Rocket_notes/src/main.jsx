17cb61856b7751985b3e2fdb63d04ed8ad63cf07
lucasspor <lucassporto.contato@gmail.com>
2024-10-16 15:19:13 -0300

    feat: adding theme with ThemeProvider  for project

--- React/Rocket_notes/src/main.jsx
+++ React/Rocket_notes/src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Details from './pages/Details/index.jsx'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.js'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details/>
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
