5871dee079798f0a1d577f6b2a138648a9766035
lucasspor <lucassporto.contato@gmail.com>
2024-10-17 15:46:02 -0300

    feat: Adding globalStyles to applicattion

--- React/Rocket_notes/src/main.jsx
+++ React/Rocket_notes/src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Details from './pages/Details/index.jsx'
+import GlobalStyles from './styles/global.js'
 import { ThemeProvider } from 'styled-components'
+
 import theme from './styles/theme.js'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 )
