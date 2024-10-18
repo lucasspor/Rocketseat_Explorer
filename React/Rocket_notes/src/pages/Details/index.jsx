c742d27dad451cc06b9918e7f903ca0f3c25188c
lucasspor <lucassporto.contato@gmail.com>
2024-10-18 17:32:36 -0300

    feat: Creating component Button

--- React/Rocket_notes/src/pages/Details/index.jsx
+++ React/Rocket_notes/src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './styles.js'
+import { Button } from '../../components/Button'
 
 export default function Details() {
   return (
     <>
       <Container>
         <h1>Hello World</h1>
+        <Button/>
       </Container>
     </>
   );
