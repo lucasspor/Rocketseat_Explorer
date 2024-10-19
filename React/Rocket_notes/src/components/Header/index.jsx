ce26442f4c93896c6fdf4a9e6a17f1c2500a3905
lucasspor <lucassporto.contato@gmail.com>
2024-10-19 09:04:19 -0300

    feat: Adding component Logout and react-icons

--- React/Rocket_notes/src/components/Header/index.jsx
+++ React/Rocket_notes/src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./style";
+import { Container, Profile, Logout } from "./style";
+import { RiShutDownLine } from 'react-icons/ri'
 
-export function Header(){
-  return(
+export function Header() {
+  return (
     <Container>
-        <img src=""/>
+      <Profile>
+        <img src="https://github.com/lucasspor.png" alt="profile image" />
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Lucas Silva Porto</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   )
 }
--- React/Rocket_notes/src/components/Header/style.js
+++ React/Rocket_notes/src/components/Header/style.js
@@ -5,7 +5,7 @@ export const Container = styled.header`
 
   height: 105px;
   width: 100%;
-  border-bottom: 1px solid ${({theme}) => theme.COLORS.BACKGROUND_700};
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
   display: flex;
   justify-content: space-between;
@@ -13,5 +13,41 @@ export const Container = styled.header`
 
   padding: 0 80px;
 
-  background-color: red;
+`
+
+export const Profile = styled.div`
+ display: flex;
+ align-items: center;
+
+ gap: 16px;
+
+ > img {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+ }
+
+ > div{
+  display: flex;
+  flex-direction: column;
+  line-height: 24px;
+
+  span{
+    font-size: 14px;
+    color: ${({theme})=> theme.COLORS.GRAY_100};
+  }
+  strong{
+    font-size: 14px;
+    color: ${({theme})=> theme.COLORS.WHITE};
+  }
+ }
+`
+export const Logout = styled.button`
+  border:0;
+  background: none;
+
+  > svg{
+    font-size: 36px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
 `
