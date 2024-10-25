dd8d96ecba619a6a68060016fc9f419609ceb693
lucasspor <lucassporto.contato@gmail.com>
2024-10-25 15:47:58 -0300

    feat: styling form to project

--- React/Rocket_notes/src/pages/Signing/index.jsx
+++ React/Rocket_notes/src/pages/Signing/index.jsx
@@ -1,10 +1,25 @@
-import { Container } from './styles'
+import { Input } from '../../components/input'
+import { FiMail, FiLock } from "react-icons/fi"
+import { Container, Form } from './styles'
+import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 
-export function Signing(){
+export function Signing() {
   return (
     <Container>
-      <h1>RocketNotes</h1>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
+        <h2>Faça seu login</h2>
+
+        <Input icon={FiMail} placeholder="E-mail"/>
+        <Input icon={FiLock} placeholder="Senha"/>
+
+        <Button title="Entrar"/>
+
+        <ButtonText title="Criar conta" isActivite/>
+      </Form>
     </Container>
   )
 
--- React/Rocket_notes/src/pages/Signing/styles.js
+++ React/Rocket_notes/src/pages/Signing/styles.js
@@ -1,8 +1,40 @@
 import styled from 'styled-components'
 
 export const Container =  styled.div`
-  height: 100%;
+  height: 100vh;
 
   display: flex;
   align-items: stretch;
+`
+
+export const Form = styled.form`
+  padding: 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  > h1{
+    font-size: 48px;
+    color: ${({theme}) => theme.COLORS.ORANGE};
+    font-weight: bold;
+  }
+  > h2 {
+    font-size: 24px;
+    color:${({theme}) => theme.COLORS.WHITE};
+    margin: 84px 0 24px;
+  }
+  
+  > p {
+    color: ${({theme}) => theme.COLORS.GRAY_100};
+    font-size: 14px;
+  }
+   
+  > button:nth-child(1n){
+    margin-top: 64px;
+  }
+
 `
