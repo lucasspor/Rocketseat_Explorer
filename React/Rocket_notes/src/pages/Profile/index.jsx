e4582cd1e53d121558f333ce96dbda8c437c93d1
lucasspor <lucassporto.contato@gmail.com>
2024-10-28 12:57:37 -0300

    feat: Adding avatar component

--- React/Rocket_notes/src/pages/Profile/index.jsx
+++ React/Rocket_notes/src/pages/Profile/index.jsx
@@ -1,8 +1,8 @@
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { FiArrowLeft, FiLock, FiMail, FiUser } from "react-icons/fi";
+import { FiArrowLeft, FiLock, FiMail, FiUser, FiCamera } from "react-icons/fi";
 
 export function Profile() {
   return (
@@ -13,6 +13,14 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/lucasspor.png" alt="Foto do usuário"/>
+
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
         <Input
           placeholder="Nome"
           type="text"
