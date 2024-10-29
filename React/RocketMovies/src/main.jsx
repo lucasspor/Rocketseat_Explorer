62407dc7846110e3ca558fbb48a942152d9a758f
lucasspor <lucassporto.contato@gmail.com>
2024-10-29 09:22:22 -0300

    feat: adding modifing

--- React/RocketMovies/src/main.jsx
+++ React/RocketMovies/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.js'
+import GlobalStyle from './styles/global.js'
+
+import { Routes } from './routes'
+import { SignIng } from './pages/SignIng/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+        <Routes/>
+    </ThemeProvider>
   </StrictMode>,
 )
