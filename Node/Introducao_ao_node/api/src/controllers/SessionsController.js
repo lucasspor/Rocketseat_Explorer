f72d90f83d9e865ff440a5f99f711fd56029154a
lucasspor <lucassporto.contato@gmail.com>
2024-11-04 12:41:20 -0300

    feat: created user validationn

--- Node/Introducao_ao_node/api/src/controllers/SessionsController.js
+++ Node/Introducao_ao_node/api/src/controllers/SessionsController.js
@@ -1,8 +1,18 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
 class SessionsController{
   async create(req, res){
     const {email, password } = req.body
 
-    return res.json({email, password})
+    const user = await knex("users").where({email}).first()
+
+    if(!user){
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+
+    return res.json(user)
   }
 
 }
