6281dcac15e4066f9fef26d155003ac8ec0ffa57
lucasspor <lucassporto.contato@gmail.com>
2024-11-11 23:05:00 -0300

    feat: Adding userAvatarController for app

--- Node/Introducao_ao_node/api/src/routes/users.routes.js
+++ Node/Introducao_ao_node/api/src/routes/users.routes.js
@@ -3,18 +3,17 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-  console.log(req.file.filename) 
-  res.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController)
 
 module.exports = usersRoutes
