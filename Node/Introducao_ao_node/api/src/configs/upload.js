8226e1865b00a27a88a614f2e23b2d01291527c4
lucasspor <lucassporto.contato@gmail.com>
2024-11-12 19:36:47 -0300

    feat: adding get method to user avatar

--- Node/Introducao_ao_node/api/src/configs/upload.js
+++ Node/Introducao_ao_node/api/src/configs/upload.js
@@ -11,7 +11,7 @@ const MULTER = {
     filename(req, file, callback){
       const fileHash = crypto.randomBytes(10).toString("hex")
 
-      const fileName = `${fileHash} - ${file.originalname}`
+      const fileName = `${fileHash}-${file.originalname}`
 
       return callback(null, fileName)
     }
