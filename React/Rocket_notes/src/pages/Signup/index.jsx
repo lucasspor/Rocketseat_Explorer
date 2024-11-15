559a6f66735d261d1863a23597b1104b819cc44e
lucasspor <lucassporto.contato@gmail.com>
2024-11-15 08:28:53 -0300

    feat: adding api connection,useState and rest to Button Compoent

--- React/Rocket_notes/src/pages/Signup/index.jsx
+++ React/Rocket_notes/src/pages/Signup/index.jsx
@@ -1,32 +1,57 @@
+import { useState } from 'react'
+
 import { Input } from '../../components/input'
 import { FiMail, FiLock, FiUser } from "react-icons/fi"
+
+import { api } from "../../services/api"
+
 import { Container, Form, Background } from "./styles.js"
 import { Button } from '../../components/Button'
-import { ButtonText } from '../../components/ButtonText'
 
 import { Link } from 'react-router-dom'
 
 export function Signup() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  function handleSignup(){
+    if(!name || !email || !password){
+      return alert("Preencha todos os dados")
+    }
+
+    api.post("/users", {name, email, password}).then(() => {
+      alert("Usuário cadastrado com sucesso").catch( error => {
+        if(error.response){
+          alert(error.response.data.message)
+        }else{
+          alert("Não foi possível cadastrar")
+        }
+      })
+    })    
+  }
+
   return (
     <Container>
-      <Background/>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
         <h2>Crie sua conta</h2>
 
-        <Input icon={FiUser} placeholder="Nome"/>
-        <Input icon={FiMail} placeholder="E-mail"/>
-        <Input icon={FiLock} placeholder="Senha"/>
+        <Input icon={FiUser} type="text" placeholder="Nome" onChange={e => setName(e.target.value)} />
+        <Input icon={FiMail} type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)}/>
+        <Input icon={FiLock} type="password" placeholder="Senha" onChange={e => setPassword(e.target.value)}/>
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={handleSignup} />
 
         <Link to="/">
           Voltar para o login
         </Link>
       </Form>
-      
+
     </Container>
   )
 
