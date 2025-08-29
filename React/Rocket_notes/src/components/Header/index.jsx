37ce07485a33f54f24bf2602a1294d6cb820a740
lucasspor <lucassporto.contato@gmail.com>
2025-08-29 19:39:34 -0300

    feat: update user avatar

--- React/Rocket_notes/src/components/Header/index.jsx
+++ React/Rocket_notes/src/components/Header/index.jsx
@@ -2,17 +2,20 @@ import { Container, Profile, Logout } from "./styles";
 import { RiShutDownLine } from 'react-icons/ri'
 
 import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
+import ProfileNoImage from '../../assets/profile-no-image.png'
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}`: ProfileNoImage
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/lucasspor.png" alt="profile image" />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem-Vindo</span>
-          <strong>Lucas Silva Porto</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
       <Logout onClick={signOut}>
--- React/Rocket_notes/src/pages/Profile/index.jsx
+++ React/Rocket_notes/src/pages/Profile/index.jsx
@@ -6,8 +6,11 @@ import { Input } from '../../components/input'
 import { Button } from '../../components/Button'
 import { FiArrowLeft, FiLock, FiMail, FiUser, FiCamera } from "react-icons/fi";
 import { Link, useNavigate } from "react-router-dom";
+import ProfileNoImage from '../../assets/profile-no-image.png'
 
 import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
+
 
 export function Profile() {
   const navigate = useNavigate()
@@ -17,6 +20,10 @@ export function Profile() {
   const [email, setEmail] = useState(user.email)
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}`: ProfileNoImage
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
 
   async function handleUpdate(event) {
     event.preventDefault()
@@ -28,12 +35,24 @@ export function Profile() {
     }
 
     try {
-      await updateProfile({ user: updatedUser })
+      await updateProfile({ user: updatedUser, avatarFile })
 
       navigate("/")
     } catch (error) {
 
     }
+  }
+
+  async function handleChangeAvatar(event){
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    if(!file){
+      return
+    }
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
   }
 
   return (
@@ -45,11 +64,15 @@ export function Profile() {
       </header>
       <Form onSubmit={handleUpdate}>
         <Avatar>
-          <img src="https://github.com/lucasspor.png" alt="Foto do usuário" />
+          <img src={avatar} alt="Foto do usuário" />
 
           <label htmlFor="avatar">
             <FiCamera />
-            <input id="avatar" type="file" />
+            <input
+             id="avatar"
+             type="file"
+             onChange={handleChangeAvatar}
+             />
           </label>
         </Avatar>
         <Input
