5a4b44101dd69de03f4c22fd7c6e87e0c76460fa
lucasspor <lucassporto.contato@gmail.com>
2025-08-31 11:53:31 -0300

    feat: add tag functionality and Enter key support

--- React/Rocket_notes/src/pages/New/index.jsx
+++ React/Rocket_notes/src/pages/New/index.jsx
@@ -16,6 +16,9 @@ export function New() {
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState("")
 
+  const [tags, setTags] = useState([])
+  const [newTag, setNewTag] = useState("")
+
   function handleAddLink() {
     if (!newLink) {
       toast.error("Preencha o link antes de adicionar");
@@ -29,12 +32,25 @@ export function New() {
     setLinks(prevLinks => prevLinks.filter((link => link !== deleted)))
   }
 
+  function handleAddTag() {
+    if (!newTag) {
+      toast.error("Preencha o link antes de adicionar");
+      return;
+    }
+    setTags(prevState => [...prevState, newTag])
+    setNewTag("")
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevTags => prevTags.filter((tag => tag !== deleted)))
+  }
+
 
   return (
     <Container>
       <Header />
       <main>
-        <Form >
+        <Form onSubmit={(e) => e.preventDefault()}>
           <header>
             <h1>Criar nota</h1>
             <Link to="/">voltar</Link>
@@ -58,12 +74,38 @@ export function New() {
               value={newLink}
               onChange={e => setNewLink(e.target.value)}
               onClick={handleAddLink}
+              onKeyDown={(e) => {
+                if (e.key == "Enter") {
+                  handleAddLink()
+                }
+              }}
             />
           </Section>
           <Section title="Marcadores">
             <div className="tags">
-              <Noteitem value="React" />
-              <Noteitem placeholder="Nova Marcação" isNew />
+              {
+                tags.map((tag, index) => {
+                  return (
+                    <Noteitem
+                      key={index}
+                      value={tag}
+                       onClick={() => handleRemoveTag(tag)}
+                    />
+                  )
+                })
+              }
+              <Noteitem
+                isNew
+                placeholder="Nova Marcação"
+                value={newTag}
+                onChange={e => setNewTag(e.target.value)}
+                onClick={handleAddTag}
+                onKeyDown={(e) => {
+                  if (e.key == "Enter") {
+                    handleAddTag()
+                  }
+                }}
+              />
             </div>
           </Section>
           <Button title="Salvar" type="submit" />
