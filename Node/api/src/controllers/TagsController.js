6c5f0866a4597ca41b05cea9fd4db1e7625fc9e3
lucasspor <lucassporto.contato@gmail.com>
2025-08-31 13:18:40 -0300

    fix: Fix user_id destructuring in TagsController

--- Node/api/src/controllers/TagsController.js
+++ Node/api/src/controllers/TagsController.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError")
 
 class TagsControllers {
   async index(req, res) {
-    const { user_id } = req.user.id
+    const  user_id  = req.user.id
 
     const checkUserExist = await knex("users").where({ id: user_id }).first();
 
