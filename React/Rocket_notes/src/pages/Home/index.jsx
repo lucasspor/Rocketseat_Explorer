22c73a9677009eecd239e0e1bbc842fba5550aa4
lucasspor <lucassporto.contato@gmail.com>
2025-08-31 20:43:39 -0300

    feat: add tag selection toggle logic

--- React/Rocket_notes/src/pages/Home/index.jsx
+++ React/Rocket_notes/src/pages/Home/index.jsx
@@ -9,35 +9,37 @@ import { Section } from "../../components/Section"
 import { Note } from "../../components/Note"
 import { api } from "../../services/api.js"
 import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
 
 export function Home() {
   const [tags, setTags] = useState([])
-  const [notes, setNotes] = useState([])
+  const [tagSelected, setTagsSelected] = useState([])
+
+function handleTagSelected(tagName){
+  const alreadySelected = tagSelected.includes(tagName)
+
+  if(alreadySelected){
+    const filteredTags = tagSelected.filter(tag => tag !== tagName)
+    setTagsSelected(filteredTags)
+  }else{
+    setTagsSelected(prevState => [...prevState, tagName])
+  }
+
+}
 
 
-
-  useEffect(() => {
-    async function fetchTags() {
-      try {
-        const response = await api.get("/tags")
-        setTags(response.data)
-      } catch (error) {
-        toast.error("Erro ao buscar tags:", error)
-      }
+useEffect(() => {
+  async function fetchTags() {
+    try {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    } catch (error) {
+      toast.error("Erro ao buscar tags:", error)
     }
-
-    async function fetchNotes() {
-      try {
-        const response = await api.get("/notes")
-        setNotes(response.data)
-      } catch (error) {
-        toast.error("Erro ao buscar notes:", error)
-      }
-    }
-
-    fetchNotes()
-    fetchTags()
-  }, [])
+  }
+  
+  fetchTags()
+}, [])
 
 
   return (
@@ -49,10 +51,17 @@ export function Home() {
       </Brand>
       <Header />
       <Menu>
-        <li><ButtonText title="Todos" isActive /></li>
+        <li><ButtonText 
+        title="Todos" 
+        isActive={setTagsSelected.length === 0} 
+        onClick={() => handleTagSelected("all")}
+        /></li>
         {tags && tags.map((tag) =>
         (
-          <li key={tag.id}><ButtonText title={tag.name} $isactivite /></li>
+          <li key={tag.id}><ButtonText 
+          title={tag.name} 
+          onClick={() => handleTagSelected(tag.name)} 
+          isActive={tagSelected.includes(tag.name)} /></li>
         ))
         }
       </Menu>
@@ -61,16 +70,12 @@ export function Home() {
       </Search>
       <Content>
         <Section title="Minhas notas">
-          {
-            notes && notes.map((note) => (
-              <Note data={{
-                title: note.title, tags: [
-                  { id: '1', name: 'express' },
-                  { id: '2', name: 'nodejs' }
-                ]
-              }} />
-            ))
-          }
+          <Note data={{
+            title: 'React', tags: [
+              { id: 1, name: 'react' },
+
+            ]
+          }} />
           <Note data={{
             title: 'Exemplo de Middleware', tags: [
               { id: '1', name: 'express' },
