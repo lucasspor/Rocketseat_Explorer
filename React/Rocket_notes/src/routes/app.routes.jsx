cfc37c72f260e21da21617f9cc3928f59541f577
lucasspor <lucassporto.contato@gmail.com>
2025-09-02 10:34:30 -0300

    feat(router): redirect all unknown routes to home

--- React/Rocket_notes/src/routes/app.routes.jsx
+++ React/Rocket_notes/src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Route , Routes} from "react-router-dom"
+import { Route , Routes, Navigate} from "react-router-dom"
 
 import { Home } from '../pages/Home'
 import { Details } from "../pages/Details"
@@ -12,6 +12,8 @@ export function AppRoutes(){
       <Route path="/new"  element={<New/>}/>
       <Route path="/profile"  element={<Profile/>}/>
       <Route path="/details/:id"  element={<Details/>}/>
+
+      <Route path="*"  element={<Navigate to="/"/>}/>
     </Routes>
   )
 }
--- React/Rocket_notes/src/routes/auth.routes.jsx
+++ React/Rocket_notes/src/routes/auth.routes.jsx
@@ -1,14 +1,18 @@
-import { Route , Routes} from "react-router-dom"
+import { Route , Routes, Navigate} from "react-router-dom"
 
 
 import { Signing } from "../pages/Signing"
 import { Signup } from "../pages/Signup"
 
 export function AuthRoutes(){
+  const user = localStorage.getItem("@rocketnote:user")
+
   return(
     <Routes>
       <Route path="/"  element={<Signing/>}/>
       <Route path="/register"  element={<Signup/>}/>
+
+      {!user && <Route path="*"  element={<Navigate to="/"/>}/>}
     </Routes>
   )
 }
