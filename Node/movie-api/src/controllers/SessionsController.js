8c5d2ee7c86d8defce30c004f42e5151e1b6b518
lucasspor <lucassporto.contato@gmail.com>
2025-09-04 08:38:20 -0300

    feat: feat: add session controller with JWT authentication middleware

---
+++ Node/movie-api/src/controllers/SessionsController.js
@@ -0,0 +1,26 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs") //importamos o compare
+
+class SessionsController{
+  async create(req, res){
+    const {email, password } = req.body
+
+    const user = await knex("users").where({email}).first()
+
+    if(!user){
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+    
+    const passwordMatched = await compare(password, user.password)
+    
+    if(!passwordMatched){
+      throw new AppError("E-mail e/ou senha incorreta", 401)
+    }
+
+    return res.json(user)
+  }
+
+}
+
+module.exports = SessionsController
