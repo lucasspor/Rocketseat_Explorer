f4bf72137f819b978f3a18690bda2d72cb56b62d
lucasspor <lucassporto.contato@gmail.com>
2025-09-13 07:20:34 -0300

    feat(home): filter feature options on homepage

--- Autenticacao_e_Autorizacao/explorer-stock/web/src/pages/Home/index.jsx
+++ Autenticacao_e_Autorizacao/explorer-stock/web/src/pages/Home/index.jsx
@@ -1,18 +1,27 @@
 import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
+import { USER_ROLE } from '../../utils/roles';
 
 import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
+import { useAuth } from '../../hooks/auth';
 
 export function Home() {
+  const { user } = useAuth()
+
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+
+        {user.role === USER_ROLE.ADMIN &&
+          <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
+        }
+        {[USER_ROLE.ADMIN, USER_ROLE.SALE].includes(user.role) &&
+          <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+        }
       </main>
     </Container>
   )
